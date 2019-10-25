fde535d43b0ffd4be8a87b725c5348b8b1a34107
Stephen McKillop <smckillop@innovada.ca>
2019-10-25 12:38:56 -0400

    Cards

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,110 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+class SecondRoute extends StatefulWidget {
+  SecondRoute({Key key}) : super(key: key);
+
+  @override
+  _SecondRouteState createState() => _SecondRouteState();
+}
+
+class _SecondRouteState extends State<SecondRoute> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Home", textAlign: TextAlign.center, style: TextStyle(color: Colors.black),),
+        backgroundColor: Colors.white,
+        leading: new IconButton(icon: new Icon(Icons.menu, color: Colors.black), onPressed: () {
+          Navigator.pop(context);
+        },),
+        actions: <Widget>[ new IconButton(icon: new Icon(Icons.favorite_border, color: Colors.black),) ],
+      ),
+      body: 
+        PageView.builder(
+          itemBuilder: (context, position) {
+            return FractionallySizedBox(widthFactor: 0.8, heightFactor: 0.8, child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter, 
+                  colors: [ 
+                    Color.fromARGB(255, 0, 173, 238),
+                    Color.fromARGB(255, 38, 210, 238)
+                   ]), 
+                borderRadius: new BorderRadius.circular(12.0),
+              ),
+              child: Column(
+                children: <Widget>[
+                  SizedBox(height: 12,),
+                  FractionallySizedBox(widthFactor: 0.6, child: Image.asset("assets/profile_title.png",)),
+                  SizedBox(height: 8,),
+                  Text("Toronto", style: 
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.w600, fontSize: 50)
+                  ),
+                  SizedBox(height: 8,),
+                  FractionallySizedBox(
+                    widthFactor: 0.9, 
+                    child: 
+                      new LinearProgressIndicator(
+                        backgroundColor: Colors.white, 
+                        value: 0.5,
+                        valueColor: new AlwaysStoppedAnimation<Color>(Color.fromARGB(255, 255, 214, 10)),
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                    SizedBox(height: 50, width: 80 , child: RaisedButton( 
+                      color: Colors.white,
+                      shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(20)
+                    ), child: Icon(Icons.fast_rewind, color: Colors.white,) ),),
+                    SizedBox(width: 8,),
+                    SizedBox(height: 50, width: 80 , child: RaisedButton( 
+                      color: Colors.white,
+                      shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(20)
+                    ), child: Icon(Icons.play_arrow, color: Colors.white,) ),),
+                    SizedBox(width: 8,),
+                    SizedBox(height: 50, width: 80 , child: RaisedButton( 
+                      color: Colors.white,
+                      shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(20)
+                    ), child: Icon(Icons.fast_forward, color: Colors.white,) ),),
+                  ],
+                  ),
+                  SizedBox(height: 8,),
+                  Text("Listen to the voice profile", style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700
+                  ),),
+                  Expanded(child: Center(
+                    child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                    SizedBox(height: 80, width: 80 , child: RaisedButton( 
+                      color: Colors.white,
+                      shape: CircleBorder(
+                        side: BorderSide(width: 2, color: Color.fromARGB(255, 0, 173, 238), style: BorderStyle.solid)
+                      ), 
+                      child: Icon(Icons.favorite, color: Colors.red, size: 50,) ),),
+                      Text("Like", style: TextStyle(fontSize: 20, color: Colors.white, ),)
+                  ],),
+                  ),)
+              ],),
+              ),
+            );
+          },
+          scrollDirection: Axis.vertical,
+        )
+    );
+  }
+}
+
 class _MyHomePageState extends State<MyHomePage> {
 
   @override
@@ -79,7 +183,12 @@ class _MyHomePageState extends State<MyHomePage> {
                       borderRadius: new BorderRadius.circular(25.0),
                       side: BorderSide(color: Color.fromARGB(100, 104, 112, 137))
                     ),
-                    onPressed: () {},
+                    onPressed: () {
+                      Navigator.push(
+                        context, 
+                        MaterialPageRoute(builder: (context) => SecondRoute())
+                      );
+                    },
                     textColor: Colors.white,
                     child: const Text("Log In", 
                       style: TextStyle(
